92e0fa7e8daf65c9bc38f4e9d721aed059860038
Juan Mosquera <juan.mosquera@systemlab.com.pe>
2015-06-07 14:40:38 -0500

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -25,8 +25,8 @@ jQuery(document).ready(function($){
   function updateSecondaryNavigation() {
     contentSections.each(function(){
       var actual = $(this),
-        actualHeight = actual.height() + parseInt(actual.css('paddingTop').replace('px', '')) + parseInt(actual.css('paddingBottom').replace('px', '')),
-        actualAnchor = secondaryNav.find('a[href="#'+actual.attr('id')+'"]');
+      actualHeight = actual.height() + parseInt(actual.css('paddingTop').replace('px', '')) + parseInt(actual.css('paddingBottom').replace('px', '')),
+      actualAnchor = secondaryNav.find('a[href="#'+actual.attr('id')+'"]');
       if ( ( actual.offset().top - secondaryNav.height() <= $(window).scrollTop() ) && ( actual.offset().top +  actualHeight - secondaryNav.height() > $(window).scrollTop() ) ) {
         actualAnchor.addClass('active');
       }else {
@@ -44,20 +44,29 @@ jQuery(document).ready(function($){
 
   //smooth scrolling when clicking on the secondary navigation items
   secondaryNav.find('ul a').on('click', function(event){
-        event.preventDefault();
-        var target= $(this.hash);
-        $('body,html').animate({
-          'scrollTop': target.offset().top - secondaryNav.height() + 1
-          }, 400
-        );
+    event.preventDefault();
+    var target= $(this.hash);
+    $('body,html').animate({
+      'scrollTop': target.offset().top - secondaryNav.height() + 1
+    }, 400
+    );
         //on mobile - close secondary navigation
         $('.lym-nav-trigger').removeClass('menu-is-open');
         secondaryNav.find('ul').removeClass('is-visible');
-    });
+      });
+
+  $('#logo-header').find('a').on('click', function(event){
+   event.preventDefault();
+   var target= $(this.hash);
+   $('body,html').animate({
+    'scrollTop': target.offset().top - secondaryNav.height() + 1
+  }, 400
+  );
+ });
 
     //on mobile - open/close primary navigation clicking/tapping the menu icon
-  $('.cd-primary-nav').on('click', function(event){
-    if($(event.target).is('.cd-primary-nav')) $(this).children('ul').toggleClass('is-visible');
-  });
+    $('.cd-primary-nav').on('click', function(event){
+      if($(event.target).is('.cd-primary-nav')) $(this).children('ul').toggleClass('is-visible');
+    });
 
-});
+  });
